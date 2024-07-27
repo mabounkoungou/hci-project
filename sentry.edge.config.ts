87d2a2f278fd488626a7f1e77d7f7ffc4079b6fd
raad <raadbouks@gmail.com>
2024-07-27 06:50:04 +0000

    Fixing Sentry

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -6,7 +6,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://3d627de24f5d06a1fc39000a06ca9a94@o4506813739368448.ingest.us.sentry.io/4507458386526208",
+  dsn: "https://664cbb8ca592a0afd6257eb08c56ec7d@o4507661749321728.ingest.us.sentry.io/4507661760200704",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
