6935f18860f45a93a6514a62d85c00aaaf222762
raad <raadbouks@gmail.com>
2024-07-27 06:38:07 +0000

    Fixing Errors

--- app/sentry-example-page/page.tsx
+++ app/sentry-example-page/page.tsx
@@ -68,9 +68,7 @@ export default function Page() {
 
         <p>
           Next, look for the error on the{" "}
-          <a href="https://javascript-mastery.sentry.io/issues/?project=4507458386526208">
             Issues Page
-          </a>
           .
         </p>
         <p style={{ marginTop: "24px" }}>
